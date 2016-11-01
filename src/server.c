70dda4f989cb9baa62f6951d8cd2213bd2c365d6
Vlad Kanash <vlad.kanash@gmail.com>
2016-11-02 00:36:30 +0300

    TIME command MSG_PEEK parsing. Code structure reworked.

--- src/server.c
+++ src/server.c
@@ -7,26 +7,42 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/sendfile.h>
 #include <netinet/in.h>
 #include <time.h>
 #include <stdbool.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include "commands.h"
+#include "types.h"
 
 #define BUF_SIZE 256
+#define COMMAND_LENGTH 16
 
 int fd_hwm = 0;
 fd_set set;
 
 void echo_message(char *in_buf, char *out_buf);
-void process_command(int fd, char *in_buf, char *out_buf);
+command_response process_command(server_command command);
+void download_file(char *buf, char *out_buf, int i);
+server_command get_command(char *buf);
+void close_connection(int fd);
+void cleanup(char *in_buf, char *out_buf, server_command *command, command_response *response);
+void input_data(int fd);
+void add_client(int fd_skt);
 
-void get_current_time(char *out) {
+bool parse_command(char *input, char command_type[5], int command_len, server_command* command);
+
+void get_current_time(command_response* result) {
+    unsigned size = 30;
     struct tm *tm;
     time_t t;
     t = time(NULL);
     tm = localtime(&t);
-    strftime(out, 30, "%F %X\n", tm);
+    result->text = malloc(sizeof(char) * size);
+    strftime(result->text , size, "%F %X\n", tm);
+    result->text_length = size;
 }
 
 bool startsWith(const char *pre, const char *str)
@@ -37,11 +53,7 @@ bool startsWith(const char *pre, const char *str)
 }
 
 void run_server(struct sockaddr_in *sap) {
-    int fd_skt, fd_client, fd;
-    char in_buf[BUF_SIZE];
-    char out_buf[BUF_SIZE];
-    memset(in_buf, 0, sizeof(in_buf));
-    memset(out_buf, 0, sizeof(out_buf));
+    int fd_skt, fd;
     fd_set read_set;
 
     fd_skt = socket(AF_INET, SOCK_STREAM, 0);
@@ -70,14 +82,9 @@ void run_server(struct sockaddr_in *sap) {
         for (fd = 0; fd <= fd_hwm; fd++) {
             if (FD_ISSET(fd, &read_set)) {
                 if (fd == fd_skt) {
-                    fd_client = accept(fd_skt, NULL, 0);
-                    FD_SET(fd_client, &set);
-                    if (fd_client > fd_hwm) {
-                        fd_hwm = fd_client;
-                    }
+                    add_client(fd_skt);
                 } else {
-                    ssize_t nread = read(fd, in_buf, sizeof(in_buf));
-                    process_command(fd, in_buf, out_buf);
+                    input_data(fd);
                 }
             }
         }
@@ -86,27 +93,140 @@ void run_server(struct sockaddr_in *sap) {
     return;
 }
 
-void process_command(int fd, char *in_buf, char *out_buf) {
-    if (strcmp(in_buf, COMMAND_TIME) == 0) {
-        get_current_time(out_buf);
-    } else if (startsWith(COMMAND_ECHO, in_buf)) {
-        echo_message(in_buf, out_buf);
-    } else if (strcmp(in_buf, COMMAND_CLOSE) == 0) {
-        FD_CLR(fd, &set);
-        if (fd == fd_hwm) {
+void add_client(int fd_skt) {
+    int fd_client = accept(fd_skt, NULL, 0);
+    FD_SET(fd_client, &set);
+    if (fd_client > fd_hwm) {
+                        fd_hwm = fd_client;
+                    }
+}
+
+void input_data(int fd) {
+    char in_buf[BUF_SIZE];
+    char out_buf[BUF_SIZE];
+    memset(in_buf, 0, sizeof(in_buf));
+    memset(out_buf, 0, sizeof(out_buf));
+    ssize_t nread = 0;
+    server_command command;
+    command_response response;
+
+    do {
+        nread = recv(fd, in_buf, sizeof(in_buf), MSG_PEEK);
+        command = get_command(in_buf);
+        if (command.success) {
+            response = process_command(command);
+            if (response.close_connection) {
+                close_connection(fd);
+            }
+            recv(fd, in_buf, command.text + command.command_length - in_buf, 0);
+            write(fd, response.text, response.text_length);
+            cleanup(in_buf, out_buf, &command, &response);
+        } else if (nread > COMMAND_LENGTH) {
+            recv(fd, in_buf, (size_t) (nread - COMMAND_LENGTH), 0);
+        }
+    } while (nread == sizeof(in_buf));
+}
+
+void cleanup(char *in_buf, char *out_buf, server_command *command, command_response *response) {
+    memset(out_buf, 0, BUF_SIZE);
+    memset(in_buf, 0, BUF_SIZE);
+    free(response->text);
+    memset(command, 0, sizeof(server_command));
+    memset(response, 0, sizeof(command_response));
+}
+
+server_command get_command(char *buf) {
+    server_command result;
+    memset(&result, 0, sizeof(result));
+    result.success = true;
+    if (parse_command(buf, COMMAND_TIME, COMMAND_TIME_LENGTH, &result)) {
+        result.type = TIME;
+        result.simple = true;
+    } else if (parse_command(buf, COMMAND_ECHO, COMMAND_ECHO_LENGTH, &result)) {
+        result.type = ECHO;
+        result.simple = false;
+    } else if (parse_command(buf, COMMAND_CLOSE, COMMAND_CLOSE_LENGTH, &result)) {
+        result.type = CLOSE;
+        result.simple = true;
+    } else if (parse_command(buf, COMMAND_DOWNLOAD, COMMAND_DOWNLOAD_LENGTH, &result)) {
+        result.type = DOWNLOAD;
+        result.simple = false;
+    } else {
+        result.success = false;
+    }
+    return result;
+}
+
+bool parse_command(char *input, char *command_type, int command_len, server_command* command) {
+    bool long_ending = false;
+    bool little_ending = false;
+    if ((command->text = strstr(input, command_type))) {
+        long_ending = command->text[command_len] == '\r' && command->text[command_len + 1] == '\n';
+        little_ending = command->text[command_len] == '\n';
+        command->command_length = (unsigned int) (long_ending ? command_len + 2 : little_ending ? command_len + 1 : 0);
+    }
+    return (command->text && (long_ending || little_ending));
+}
+
+command_response process_command(server_command command) {
+    command_response result;
+    memset(&result, 0, sizeof(result));
+    if (command.type == TIME) {
+        get_current_time(&result);
+        result.text_length = 30;
+        result.success = true;
+        result.close_connection = false;
+    } else if (command.type == CLOSE) {
+        result.close_connection = true;
+        result.success = true;
+    } else {
+        result.success = false;
+        result.close_connection = false;
+    }
+    return result;
+}
+
+void close_connection(int fd) {
+    FD_CLR(fd, &set);
+    if (fd == fd_hwm) {
             fd_hwm--;
         }
+    close(fd);
+}
+
+void download_file(char *buf, char *out_buf, int fd) {
+    int offset;
+    ssize_t sent_bytes;
+    __off_t remain_data;
+    struct stat file_stat;
+    memmove(buf, buf + sizeof(COMMAND_DOWNLOAD) - 1, BUF_SIZE);
+    buf[strlen(buf) - 1] = '\0';
+
+    int file = open(buf, O_RDONLY);
+    if (file == -1) {
         memset(out_buf, 0, BUF_SIZE);
-        close(fd);
-    } else {
-        memset(out_buf, 0, BUF_SIZE);
+        sprintf(out_buf, "File %s cannot be found on server", buf);
+        perror("Can't open file");
+        return;
     }
-    memset(in_buf, 0, BUF_SIZE);
-    write(fd, out_buf, BUF_SIZE);
+    if (fstat(file, &file_stat) < 0) {
+        perror("fstat error");
+        return;
+    }
+    fprintf(stdout, "File size = %li bytes", file_stat.st_size);
+
+    offset = 0;
+    remain_data = file_stat.st_size;
+    while ((sent_bytes = sendfile(fd, file, (off_t *) &offset, BUF_SIZE)) > 0) {
+        fprintf(stdout, "Server sent %zi bytes from file's data, offset is now : %d and remaining data is  %li\n",
+        sent_bytes, offset, remain_data);
+        remain_data -= sent_bytes;
+    }
 }
 
 void echo_message(char *in_buf, char *out_buf) {
     memcpy(out_buf, in_buf, BUF_SIZE);
     memmove(out_buf, out_buf+5, BUF_SIZE);
 }
+
 
---
+++ src/types.h
@@ -0,0 +1,27 @@
+//
+// Created by vladkanash on 1.11.16.
+//
+
+#ifndef LANSS_TYPES_H
+#define LANSS_TYPES_H
+
+#include <stdbool.h>
+
+typedef enum commands {TIME, ECHO, CLOSE, DOWNLOAD} commands;
+
+typedef struct server_command {
+    char* text;
+    bool simple;
+    bool success;
+    commands type;
+    size_t command_length;
+} server_command;
+
+typedef struct command_response {
+    char* text;
+    unsigned text_length;
+    bool close_connection;
+    bool success;
+} command_response;
+
+#endif //LANSS_TYPES_H
