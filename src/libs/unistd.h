ac2fb0beeee6fe3bbb20135a187340c3a164e945
Daniil Kachur <daniil.field@yandex.ru>
2016-11-05 20:04:45 +0300

    add unix-like libs

---
+++ src/libs/unistd.h
@@ -0,0 +1,29 @@
+#ifndef _UNISTD_H
+#define _UNISTD_H    1
+
+#ifdef _WIN32
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
+
+#if defined(_WIN32)
+ 
+#include <stdint.h>
+ 
+typedef uint8_t u_int8_t;
+typedef uint16_t u_int16_t;
+typedef uint32_t u_int32_t;
+ 
+#endif
+
+#ifndef _SSIZE_T_DEFINED
+#ifdef  _WIN32
+typedef unsigned __int32    ssize_t;
+#else
+typedef _W32 unsigned int   ssize_t;
+#endif
+#define _SSIZE_T_DEFINED
+#endif
+
+#endif /* unistd.h  */
