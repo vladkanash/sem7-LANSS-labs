3fbfbcca6f0633f905d87563d03e69742b3859f0
Mikhail Dyomin <mikhaildyomin@gmail.com>
2016-11-15 20:39:03 +0300

    Add some server tests

--- test/server_tests/server_test.c
+++ test/server_tests/server_test.c
@@ -7,21 +7,65 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-/* A test case that does nothing and succeeds. */
-static void null_test_success(void **state) {
-    (void) state; /* unused */
-    assert_false(0);
+#include <string.h>
+
+#include "../src/server/engine.h"
+#include "../src/constants.h"
+
+static command_holder command_info[COMMAND_COUNT];
+
+static void init_commands_test() {
+  init_commands();
+
+  int commands_count = sizeof(command_info)/sizeof(command_info[0]);
+  assert_int_equal(4, commands_count);
 }
 
-static void null_test_error(void **state) {
-    assert_false(1);
+static void parse_command_test() {
+  server_command command;
+  memset(&command, 0, sizeof(server_command));
+  command.state = INITIAL;
+  command.success = false;
+
+  char input[] = "TIME\n";
+
+  bool result = parse_command(input, &command);
+
+  assert_true(result);
+}
+
+static void get_command_test() {
+  char buf[] = "TIME\n";
+
+  server_command response = get_command(buf);
+
+  assert_string_equal(response.text, buf);
+}
+
+static void process_command_test() {
+  char buf[] = "TIME\n";
+  server_command command = get_command(buf);
+
+  command_response result = process_command(command);
+
+  assert_true(result.success);
+}
+
+static void find_line_ending_test() {
+  char buf[] = "TIME\n";
+  int result = find_line_ending(buf);
+
+  assert_int_not_equal(result, 0);
 }
 
 int main(void) {
-    const struct CMUnitTest tests[] = {
-            cmocka_unit_test(null_test_success),
-            cmocka_unit_test(null_test_error),
-    };
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(init_commands_test),
+    cmocka_unit_test(parse_command_test),
+    cmocka_unit_test(get_command_test),
+    cmocka_unit_test(process_command_test),
+    cmocka_unit_test(find_line_ending_test),
+  };
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+  return cmocka_run_group_tests(tests, NULL, NULL);
 }
